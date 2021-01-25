8690e02eeb092d3968ea244c644cdf2d9dbecc22
marieStamp <mariemarkina@gmail.com>
2021-01-26 02:18:57 +0300

    index

---
+++ public/index.php
@@ -0,0 +1,11 @@
+<?php
+// Задание 2
+$name = "Maria";
+$question = "How are You? <br>";
+$userAge = "How old are You?";
+echo "Hello, $name! $question $userAge <br>";
+
+define('MY_CONST', 100);
+echo MY_CONST;
+?>
+
