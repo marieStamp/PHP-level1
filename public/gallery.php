64219f8ea1dad22ad0205c3112aa5c2ab99c7f65
marieStamp <mariemarkina@gmail.com>
2021-02-05 14:46:11 +0300

    homework 4

---
+++ public/gallery.php
@@ -0,0 +1,6 @@
+<?php
+include_once __DIR__ . "/../config/main.php";
+include_once ENGINE_DIR . "filesystem.php";
+$images = getFiles(PUBLIC_DIR . "img");
+include VIEWS_DIR . "gallery/gallery.php";
+?>
